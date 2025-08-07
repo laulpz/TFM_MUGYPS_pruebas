d3efe734b0ac5dcd7b41d6f390e2cd648167de2a
laulpz <51860984+laulpz@users.noreply.github.com>
2025-08-07 19:56:12 +0200

    Delete pages directory

--- pages/2_Generador_Demanda.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from generador_demanda import generar_demanda_interactiva
-
-st.set_page_config(page_title="Generador de Demanda", layout="wide")
-st.title("🗓️ Generador de Demanda de Turnos")
-generar_demanda_interactiva()
