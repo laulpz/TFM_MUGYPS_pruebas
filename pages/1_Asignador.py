a5520b978258a2fc20a724769296a310996bc12c
laulpz <51860984+laulpz@users.noreply.github.com>
2025-08-07 19:29:00 +0200

    Update 1_Asignador.py

--- pages/1_Asignador.py
+++ pages/1_Asignador.py
@@ -231,6 +231,14 @@ if file_staff:
 
         st.subheader("📊 Resumen mensual")
         st.dataframe(resumen_mensual)
+
+        def to_excel_bytes(df):
+            output = BytesIO()
+            with pd.ExcelWriter(output, engine="openpyxl", date_format="DD/MM/YYYY") as writer:
+                df.to_excel(writer, index=False)
+            return output.getvalue()
+
+
         # Mostrar botones solo si los DataFrames siguen disponibles en sesión
         if "df_assign" in st.session_state:
             st.download_button(
@@ -246,12 +254,7 @@ if file_staff:
                 file_name="Resumen_Mensual.xlsx"
             )
 
-        def to_excel_bytes(df):
-            output = BytesIO()
-            with pd.ExcelWriter(output, engine="openpyxl", date_format="DD/MM/YYYY") as writer:
-                df.to_excel(writer, index=False)
-            return output.getvalue()
-
+        
         st.download_button("⬇️ Descargar planilla asignada", data=to_excel_bytes(df_assign), file_name="Planilla_Asignada.xlsx")
         st.download_button("⬇️ Descargar resumen mensual", data=to_excel_bytes(resumen_mensual), file_name="Resumen_Mensual.xlsx")
 
